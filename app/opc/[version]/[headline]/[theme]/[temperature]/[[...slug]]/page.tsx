0cbffd244fc7acab3b136ef76c27a8d91ded2353
jacqueline spineli <pp.spineli@gmail.com>
2026-08-25 11:02:35 -0300

    lp versão v5 com apenas uma dobra

--- app/opc/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
+++ app/opc/[version]/[headline]/[theme]/[temperature]/[[...slug]]/page.tsx
@@ -2,12 +2,20 @@
 
 import SplashScreenOdp from "@/app/components/SplashScreenOdp";
 import Formv4 from "@/app/opc/[version]/v4";
+import Formv5 from "@/app/opc/[version]/v5";
+import { useParams } from "next/navigation";
 
 export default function Home() {
+	const params = useParams();
+	const version = Array.isArray(params.version)
+		? params.version[0]
+		: params.version;
 
-  return (
-    <SplashScreenOdp>
-      <Formv4 />
-    </SplashScreenOdp>
-  );
+	const Form = version === "v5" ? Formv5 : Formv4;
+
+	return (
+		<SplashScreenOdp>
+			<Form />
+		</SplashScreenOdp>
+	);
 }
--- app/opc/[version]/page.tsx
+++ app/opc/[version]/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
 		);
 	}
 
+
 	return (
 		<SplashScreenOdp>
 			<Formv1
