da709cb50735fab3fb521881f010ec50128c6cbc
tialiancadivergente <ti@aliancadivergente.com.br>
2026-08-27 11:22:00 -0300

    BuzzLead

--- app/opc/[version]/v10/hero-section.tsx
+++ app/opc/[version]/v10/hero-section.tsx
@@ -47,7 +47,7 @@ export default function HeroSection({
 							Preencha abaixo e gere seu link de indicação:
 						</div>
 
-						<div className="absolute z-50" id="buzzlead-root"></div>
+						<div className="top-[480px]" id="buzzlead-root"></div>
 					</div>
 				</div>
 			</section>
