00881a62bdf95cf0f45ec1ea2c2aa60e88455004
tialiancadivergente <ti@aliancadivergente.com.br>
2026-08-28 23:41:19 -0300

    ajuste GTM org e o

--- app/components/GoogleTagManager.tsx
+++ app/components/GoogleTagManager.tsx
@@ -5,16 +5,32 @@ import { useParams } from "next/navigation";
 import TagManager from "react-gtm-module";
 
 const GoogleTagManager = () => {
-  const params = useParams<{ version?: string | string[] }>();
+  const params = useParams<{ 
+    version?: string | string[],
+    temperature?: string | string[],
+  }>();
 
   useEffect(() => {
     const version = Array.isArray(params.version)
       ? params.version[0]
       : params.version;
-    const gtmId = version === "v5" ? "GTM-MHC745NZ" : "";
+
+    const temperature = Array.isArray(params.temperature)
+      ? params.temperature[0]
+      : params.temperature;
+
+      console.log('temperature', temperature)
+
+    const normalizeTemperature = temperature === "org" || temperature === "o"
+
+    const gtmId = version === "v5" && !normalizeTemperature ? "GTM-MHC745NZ" : "";
+
+    console.log('gtmId', gtmId)
 
     if (!gtmId) return;
-    
+
+    console.log('gtmId', gtmId)
+
     TagManager.initialize({ gtmId });
   }, [params.version]);
 
